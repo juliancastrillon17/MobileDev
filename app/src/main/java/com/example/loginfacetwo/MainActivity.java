18bb4810227c18f53b4969909d387e47e94964ec
Julián <julian.castrillon234@pascualbravo.edu.co>
2023-03-13 22:53:01 -0500

    SharedPreference.

--- app/src/main/java/com/example/loginfacetwo/MainActivity.java
+++ app/src/main/java/com/example/loginfacetwo/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.loginfacetwo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btn = findViewById(R.id.sing);
+        Button sv = findViewById(R.id.save);
         users = findViewById(R.id.user);
         emails = findViewById(R.id.email);
         passwords = findViewById(R.id.password);
@@ -32,6 +35,45 @@ public class MainActivity extends AppCompatActivity {
                 sing(view);
             }
         });
+
+        SharedPreferences preferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+        SharedPreferences preferencesE = getSharedPreferences("credencialesE", Context.MODE_PRIVATE);
+        SharedPreferences preferencesP = getSharedPreferences("credencialesP", Context.MODE_PRIVATE);
+
+
+        users.setText(preferences.getString("user",""));
+        emails.setText(preferencesE.getString("email",""));
+        passwords.setText(preferencesP.getString("password",""));
+
+
+        sv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                guardar(view);
+            }
+        });
+    }
+
+    public void guardar(View view){
+        SharedPreferences preferencias = getSharedPreferences("credenciales",Context.MODE_PRIVATE);
+        SharedPreferences preferenciasE = getSharedPreferences("credencialesE",Context.MODE_PRIVATE);
+        SharedPreferences preferenciasP = getSharedPreferences("credencialesP",Context.MODE_PRIVATE);
+
+
+        SharedPreferences.Editor Object_editor = preferencias.edit(); // Hereda las propiedades de la clase y permite modificar la info a guardar;
+        // El método Edit guarda la información modificada;
+        SharedPreferences.Editor ObjectE_editor = preferenciasE.edit();
+        SharedPreferences.Editor ObjectP_editor = preferenciasP.edit();
+
+        Object_editor.putString("user",users.getText().toString());
+        ObjectE_editor.putString("email",emails.getText().toString());
+        ObjectP_editor.putString("password",passwords.getText().toString());
+
+        Object_editor.commit(); // Vaciado de la información;
+        ObjectE_editor.commit(); // Vaciado de la información;
+        ObjectP_editor.commit(); // Vaciado de la información;
+
+        finish();
     }
 
     public void sing(View view){
